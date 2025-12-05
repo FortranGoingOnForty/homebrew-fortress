56149a63886c6e44e9a728e69ad16e94ff28c104
espadonne <espadonne@outlook.com>
2025-12-05 18:55:09 +0000

    Update to v1.0.0 - Fix top status bar rendering, merge favorites features

--- Formula/fortress.rb
+++ Formula/fortress.rb
@@ -1,8 +1,8 @@
 class Fortress < Formula
   desc "Command-line file explorer written in modern Fortran with cd-on-exit"
   homepage "https://github.com/FortranGoingOnForty/fortress"
-  url "https://github.com/FortranGoingOnForty/fortress/archive/v0.9.99.tar.gz"
-  sha256 "5eb08ae980e3e4d3469510eda0c5d325db831ab8f3ac3146befd51efa929245f"
+  url "https://github.com/FortranGoingOnForty/fortress/archive/v1.0.0.tar.gz"
+  sha256 "277a4ee661efb13af61ef9bf49b068fc9dc2e9f39b6797aa5e52ff4d70edda11"
   license "MIT"
 
   depends_on "fpm" => :build
